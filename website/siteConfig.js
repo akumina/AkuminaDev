27ac5535a4f4d65d48a6f6667d5e4fe62c504009
Sean Glover <sean.glover@akumina.com>
2019-07-19 16:44:14 -0400

    Collapsable sidebars

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -95,6 +95,7 @@ const siteConfig = {
 
   // Show documentation's last update time.
   // enableUpdateTime: true,
+  docsSideNavCollapsible: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
